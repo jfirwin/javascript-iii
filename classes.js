382890727a89fe7817cc86d588d9f1deeb487310
Foster Irwin <jfosterirwin@Fosters-MacBook-Air.local>
2017-08-22 20:31:18 -0600

    created ProgressiveManager class and added functionality to hire & fire

--- classes.js
+++ classes.js
@@ -93,7 +93,41 @@ Everytime they fire an employee they get a bonus of $100 add to their .
 call you class ProgressiveManager
 */
 
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = [];
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget';
+  }
+  hire(employee) {
+    this.reports.push(employee);
+    if (this.reports.length === 0) {
+      this.title = 'Not a manager';
+    } else if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = 'Barely Manager';
+    } else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      this.title = 'Mostly Manager';
+    } else if (this.reports.length >= 11 && this.reports.length <= 50) {
+      this.title = 'Manager';
+    } else if (this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = 'Manager Plus';
+    } else if (this.reports.length <= 101) {
+      this.title = 'Bestest Manager';
+    }
+  }
+  fire(index) {
+    this.reports.splice(index, 1);
+    this.bonus += 100;
+  }
 
+}
 
 
 /*

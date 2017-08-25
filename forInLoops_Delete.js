c2f4a3edc4aaa0b6874c4627d0842930797d76f9
Foster Irwin <jfosterirwin@Fosters-MacBook-Air.local>
2017-08-24 19:21:07 -0600

    created secrets function

--- forInLoops_Delete.js
+++ forInLoops_Delete.js
@@ -73,7 +73,16 @@ function double(obj) {
 
 // Write a function called secrets that will take in an object. Create an empty string variable. Write a for in loop that loops over the object. If the property name starts with an 'sh', concatenate the value to the string variable. By the end of the for in loop, you should have a sentence, return that sentence.
 
-// CODE HERE
+function secrets(obj) {
+  var str = ''
+  for (var key in obj) {
+    let substring = key.substring(0, 2);
+    if (substring === 'sh') {
+      str += obj[key];
+    }
+  }
+  return str;
+}
 
 
 // ========================
